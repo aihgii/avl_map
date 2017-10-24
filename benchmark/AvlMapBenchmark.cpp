db20890449b40d0436e82ab67f7bc38cbb6e4c3e
Eugene <eugene.a.weiss@gmail.com>
2017-10-24 11:33:12 +0600

    Changing benchmark measurement from range to item

--- benchmark/AvlMapBenchmark.cpp
+++ benchmark/AvlMapBenchmark.cpp
@@ -7,153 +7,152 @@ class MapBenchmarkFixture : public benchmark::Fixture { };
 
 BENCHMARK_DEFINE_F(MapBenchmarkFixture, RbMapOrderedInsertion)(benchmark::State& state) {
     std::map<int,int> m;
+    for (int i = 0; i < state.range(0); i++)
+        m.emplace(i,i);
+
     while (state.KeepRunning())
-        for (int i = 0; i < state.range(0); i++)
-            m.emplace(i,i);
+        m.emplace(state.range(0),state.range(0));
 }
 BENCHMARK_REGISTER_F(MapBenchmarkFixture, RbMapOrderedInsertion)
         ->RangeMultiplier(2)
-        ->Range(8<<10,1024<<10)
-        ->Unit(benchmark::kMillisecond);
+        ->Range(1<<10,1024<<10);
 
 BENCHMARK_DEFINE_F(MapBenchmarkFixture, AvlMapOrderedInsertion)(benchmark::State& state) {
     not_std::map<int,int> m;
+    for (int i = 0; i < state.range(0); i++)
+        m.emplace(i,i);
+
     while (state.KeepRunning())
-        for (int i = 0; i < state.range(0); i++)
-            m.emplace(i,i);
+        m.emplace(state.range(0),state.range(0));
 }
 BENCHMARK_REGISTER_F(MapBenchmarkFixture, AvlMapOrderedInsertion)
         ->RangeMultiplier(2)
-        ->Range(8<<10,1024<<10)
-        ->Unit(benchmark::kMillisecond);
+        ->Range(1<<10,1024<<10);
 
 BENCHMARK_DEFINE_F(MapBenchmarkFixture, RbMapRandomInsertion)(benchmark::State& state) {
     std::random_device rd;
     std::mt19937 gen(rd());
-    std::set<int> s;
     std::map<int,int> m;
 
-    while(s.size() < state.range(0))
-        s.emplace(gen());
-
-    while (state.KeepRunning()) {
-        for (auto it = s.begin(); it != s.end(); it++)
-            m.emplace(*it, *it);
+    while(m.size() < state.range(0)) {
+        auto num = gen();
+        m.emplace(num,num);
     }
+
+    auto num = gen();
+    while(m.count(num))
+        num = gen();
+
+    while (state.KeepRunning())
+        m.emplace(num, num);
 }
 BENCHMARK_REGISTER_F(MapBenchmarkFixture, RbMapRandomInsertion)
         ->RangeMultiplier(2)
-        ->Range(8<<10,1024<<10)
-        ->Unit(benchmark::kMillisecond);
+        ->Range(1<<10,1024<<10);
 
 BENCHMARK_DEFINE_F(MapBenchmarkFixture, AvlMapRandomInsertion)(benchmark::State& state) {
     std::random_device rd;
     std::mt19937 gen(rd());
-    std::set<int> s;
     not_std::map<int,int> m;
 
-    while(s.size() < state.range(0))
-        s.emplace(gen());
+    while(m.size() < state.range(0)) {
+        auto num = gen();
+        m.emplace(num,num);
+    }
+
+    auto num = gen();
+    while(m.count(num))
+        num = gen();
 
     while (state.KeepRunning())
-        for (auto it = s.begin(); it != s.end(); it++)
-            m.emplace(*it, *it);
+        m.emplace(num, num);
 }
 BENCHMARK_REGISTER_F(MapBenchmarkFixture, AvlMapRandomInsertion)
         ->RangeMultiplier(2)
-        ->Range(8<<10,1024<<10)
-        ->Unit(benchmark::kMillisecond);
+        ->Range(1<<10,1024<<10);
 
 BENCHMARK_DEFINE_F(MapBenchmarkFixture, RbMapAccess)(benchmark::State& state) {
     std::random_device rd;
     std::mt19937 gen(rd());
-    std::set<int> s;
     std::map<int,int> m;
 
-    while(s.size() < state.range(0))
-    {
-        auto key = gen();
-        if (s.emplace(key).second)
-            m.emplace(key,key);
+    while(m.size() < state.range(0)) {
+        auto num = gen();
+        m.emplace(num,num);
     }
 
-    while (state.KeepRunning()) {
-        for (auto it = s.begin(); it != s.end(); it++)
-            auto res = m[*it];
-    }
+    auto num = gen();
+    while(!m.count(num))
+        num = gen();
+
+    while (state.KeepRunning())
+        auto res = m[num];
 }
 BENCHMARK_REGISTER_F(MapBenchmarkFixture, RbMapAccess)
         ->RangeMultiplier(2)
-        ->Range(8<<10,1024<<10)
-        ->Unit(benchmark::kMillisecond);
+        ->Range(1<<10,1024<<10);
 
 BENCHMARK_DEFINE_F(MapBenchmarkFixture, AvlMapAccess)(benchmark::State& state) {
     std::random_device rd;
     std::mt19937 gen(rd());
-    std::set<int> s;
     not_std::map<int,int> m;
 
-    while(s.size() < state.range(0))
-    {
-        auto key = gen();
-        if (s.emplace(key).second)
-            m.emplace(key,key);
+    while(m.size() < state.range(0)) {
+        auto num = gen();
+        m.emplace(num,num);
     }
 
-    while (state.KeepRunning()) {
-        for (auto it = s.begin(); it != s.end(); it++)
-            auto res = m[*it];
-    }
+    auto num = gen();
+    while(!m.count(num))
+        num = gen();
+
+    while (state.KeepRunning())
+        auto res = m[num];
 }
 BENCHMARK_REGISTER_F(MapBenchmarkFixture, AvlMapAccess)
         ->RangeMultiplier(2)
-        ->Range(8<<10,1024<<10)
-        ->Unit(benchmark::kMillisecond);
+        ->Range(1<<10,1024<<10);
 
 BENCHMARK_DEFINE_F(MapBenchmarkFixture, RbMapErase)(benchmark::State& state) {
     std::random_device rd;
     std::mt19937 gen(rd());
-    std::set<int> s;
     std::map<int,int> m;
 
-    while(s.size() < state.range(0))
-    {
-        auto key = gen();
-        if (s.emplace(key).second)
-            m.emplace(key,key);
+    while(m.size() < state.range(0)) {
+        auto num = gen();
+        m.emplace(num,num);
     }
 
-    while (state.KeepRunning()) {
-        for (auto it = s.begin(); it != s.end(); it++)
-            m.erase(*it);
-    }
+    auto num = gen();
+    while(!m.count(num))
+        num = gen();
+
+    while (state.KeepRunning())
+            m.erase(num);
 }
 BENCHMARK_REGISTER_F(MapBenchmarkFixture, RbMapErase)
         ->RangeMultiplier(2)
-        ->Range(8<<10,1024<<10)
-        ->Unit(benchmark::kMillisecond);
+        ->Range(1<<10,1024<<10);
 
 BENCHMARK_DEFINE_F(MapBenchmarkFixture, AvlMapErase)(benchmark::State& state) {
     std::random_device rd;
     std::mt19937 gen(rd());
-    std::set<int> s;
     not_std::map<int,int> m;
 
-    while(s.size() < state.range(0))
-    {
-        auto key = gen();
-        if (s.emplace(key).second)
-            m.emplace(key,key);
+    while(m.size() < state.range(0)) {
+        auto num = gen();
+        m.emplace(num,num);
     }
 
-    while (state.KeepRunning()) {
-        for (auto it = s.begin(); it != s.end(); it++)
-            m.erase(*it);
-    }
+    auto num = gen();
+    while(!m.count(num))
+        num = gen();
+
+    while (state.KeepRunning())
+        m.erase(num);
 }
 BENCHMARK_REGISTER_F(MapBenchmarkFixture, AvlMapErase)
         ->RangeMultiplier(2)
-        ->Range(8<<10,1024<<10)
-        ->Unit(benchmark::kMillisecond);
+        ->Range(1<<10,1024<<10);
 
 BENCHMARK_MAIN();
